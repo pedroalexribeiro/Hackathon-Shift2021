15c66a82f6917cdfe545cd6f9ec8ec6a783519ce
GonzagaZZZ <mike.gonzaga1998@gmail.com>
2021-04-16 20:42:27 +0100

    Add DB Manager

--- flutter/raven/lib/main.dart
+++ flutter/raven/lib/main.dart
@@ -1,83 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:raven/db_manager.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MainApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Raven - Heaven's Courier",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Test(),
+      home: MainPage(),
     );
   }
+
 }
 
-class Test extends StatefulWidget {
-  Test();
+class MainPage extends StatefulWidget {
+
   @override
-  _Test createState() => _Test();
+  _MainPage createState() => _MainPage();
 }
 
-class _Test extends State<Test> {
-  /*late Future<http.Response> info;*/
+class _MainPage extends State<MainPage> {
 
   @override
   void initState() {
     super.initState();
-    //info = getInformation();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-        appBar: AppBar(
-      // Here we take the value from the MyHomePage object that was created by
-      // the App.build method, and use it to set our appbar title.
-      title: Text("Fuck you."),
-    ));
+      appBar: AppBar(
+        title: Text("Fuck you."),
+      ),
+      body: TextButton(
+        child: Text("Send request"),
+        onPressed: () async {
+            var response = await DBManager().getInfo();
+            debugPrint(response.body);
+          },
+      ),);
   }
-}
-
-Future<http.Response> getInformation() async {
-  final response = await http.get(Uri.https('127.0.0.1', 'requests'));
-
-  return response;
-  /*if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }*/
 }
 
 class Enterprises {
